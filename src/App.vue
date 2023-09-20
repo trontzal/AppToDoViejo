034c875cfecd861a667f62995322ae28788e271f
trontzal <trontzal@gmail.com>
2023-09-20 18:24:22 +0200

    todo bien, falta funcionadlidad

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,8 @@ export default defineComponent({
   name: 'App'
 })
 </script>
+<style>
+.text-strikethrough {
+  text-decoration: line-through;
+}
+</style>
--- src/pages/pageToDo.vue
+++ src/pages/pageToDo.vue
@@ -2,24 +2,33 @@
   <q-page class="q-pa-md">
     <p>ToDo</p>
     <q-item-label header>General</q-item-label>
-    <q-list bordered>
+    <q-list bordered separator>
     <q-item
       v-ripple
       v-for="task in tasks"
       :key="task.id"
       clickable
-      @click="task.completed = !task.completed">
+      @click="task.completed = !task.completed"
+      :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'">
 
       <q-item-section side top>
         <q-checkbox v-model="task.completed" />
       </q-item-section>
 
       <q-item-section>
-        <q-item-label>{{ task.name }}</q-item-label>
+        <q-item-label :class="{'text-strikethrough' : task.completed}">{{ task.name }}</q-item-label>
       </q-item-section>
-      <q-item-section side top>
-        <q-item-label caption>{{ task.dueDate }}</q-item-label>
-        <q-item-label caption>{{ task.dueTime }}</q-item-label>
+      <q-item-section side>
+        <div class="row">
+          <div class="collum justify-center">
+            <q-icon name="event" size="18px" class="q-mr-xs"/>
+          </div>
+          <div class="collum">
+            <q-item-label caption class="row justify-end">{{ task.dueDate }}</q-item-label>
+            <q-item-label caption class="row justify-end">{{ task.dueTime }}</q-item-label>
+          </div>
+        </div>
+
       </q-item-section>
     </q-item>
     </q-list>
